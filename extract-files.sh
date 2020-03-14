a8e17b5f6057dac86706237bef6aa4bc566d49e2
JacobDeepu <d2jdeepu@gmail.com>
2020-03-14 23:04:56 +0530

    [Patch-FP]mido: Patch Goodix blobs to load custom libbinder
    - Add custom libbinder libbindergx.so
    - Replace libbinder.so with custom libbinder in biometrics hal
    
    Co-authored-by: ZeeLog <Danjel.Zammit@gmail.com>
    Signed-off-by: JacobDeepu <d2jdeepu@gmail.com>
    (cherry picked from commit b55bc73b58242f9181fa3dc610808503ad8295dd)

--- extract-files.sh
+++ extract-files.sh
@@ -17,6 +17,12 @@
 
 set -e
 
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+MK_ROOT="$MY_DIR"/../../..
+
 # Required!
 export DEVICE=mido
 export DEVICE_COMMON=msm8953-common
@@ -25,3 +31,11 @@ export VENDOR=xiaomi
 export DEVICE_BRINGUP_YEAR=2020
 
 ./../../$VENDOR/$DEVICE_COMMON/extract-files.sh $@
+# Hex for goodix fingerprint
+DEVICE_BLOB_ROOT="$MK_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
+patchelf --replace-needed libbinder.so libbindergx.so "$DEVICE_BLOB_ROOT"/vendor/bin/gx_fpd
+patchelf --replace-needed libbinder.so libbindergx.so "$DEVICE_BLOB_ROOT"/vendor/lib64/hw/fingerprint.goodix.so
+patchelf --replace-needed libbinder.so libbindergx.so "$DEVICE_BLOB_ROOT"/vendor/lib64/libfp_client.so
+patchelf --replace-needed libbinder.so libbindergx.so "$DEVICE_BLOB_ROOT"/vendor/lib64/libfpservice.so
+
